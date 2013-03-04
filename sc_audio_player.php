0fbfdba840221bd38c2879b36a05569a9a75c4a1
mra13 <mra13@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-04 04:02:26 +0000

    tagging v1.2
    git-svn-id: https://plugins.svn.wordpress.org/compact-wp-audio-player/tags/1.2@675841 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ sc_audio_player.php
@@ -0,0 +1,109 @@
+<?php
+/*
+Plugin Name: Compact Audio Player
+Description: Plays a specified audio file (.mp3 or .ogg) using a simple and compact audio player. The audio player is compatible with all major browsers and devices (Android, iPhone).
+Version: 1.2
+Author: Tips and Tricks HQ
+Author URI: http://www.tipsandtricks-hq.com/
+License: GPL2  
+*/
+
+define('SC_AUDIO_BASE_URL', plugins_url('/',__FILE__));
+
+add_action('init', 'wp_sc_audio_init');
+function wp_sc_audio_init()
+{
+	if (!is_admin()){
+		wp_register_script('scap.soundmanager2', SC_AUDIO_BASE_URL.'js/soundmanager2-nodebug-jsmin.js');
+		wp_enqueue_script('scap.soundmanager2');
+		wp_register_style('scap.flashblock', SC_AUDIO_BASE_URL.'css/flashblock.css');
+        wp_enqueue_style('scap.flashblock');
+		wp_register_style('scap.player', SC_AUDIO_BASE_URL.'css/player.css');
+        wp_enqueue_style('scap.player');
+	}
+}
+
+function scap_footer_code(){
+	?>
+	<script type="text/javascript">
+    soundManager.useFlashBlock = true; // optional - if used, required flashblock.css
+    soundManager.url = '<?php echo SC_AUDIO_BASE_URL; ?>swf/soundmanager2.swf';
+    function play_mp3(flg,ids,mp3url){
+      soundManager.createSound({
+        id:'btnplay_'+ids,
+        url: mp3url
+      });
+	
+      if(flg == 'play'){
+		//soundManager.stopAll();        
+		soundManager.play('btnplay_'+ids,{
+			  	onfinish: function() {
+			  	document.getElementById('btnplay_'+ids).style.display = 'inline';
+          document.getElementById('btnstop_'+ids).style.display = 'none';
+			  	}
+			  });
+      }
+      else if(flg == 'stop'){
+        //soundManager.stop('btnplay_'+ids);
+    	soundManager.pause('btnplay_'+ids);
+      }
+    }
+    function show_hide(flag,ids){
+      if(flag=='play'){
+        document.getElementById('btnplay_'+ids).style.display = 'none';
+        document.getElementById('btnstop_'+ids).style.display = 'inline';
+      }
+      else if (flag == 'stop'){
+        document.getElementById('btnplay_'+ids).style.display = 'inline';
+        document.getElementById('btnstop_'+ids).style.display = 'none';
+      }
+    }
+	</script>
+	<?php
+}
+add_action('wp_footer', 'scap_footer_code');
+
+function sc_embed_player_handler($atts, $content = null) 
+{
+	extract(shortcode_atts(array(
+		'fileurl' => '',
+		'class' => '',
+	), $atts));	
+	if(empty($fileurl)){
+		return '<div style="color:red;font-weight:bold;">Compact Audio Player Error! You must enter the mp3 file URL via the "fileurl" parameter in this shortcode. Please check the documentation and correct the mistake.</div>';
+	}
+	if(empty($class)){$class = "sc_player_container1";}//Set default container class
+	$ids = microtime();
+	$ids= str_replace(' ','',$ids);
+	$ids= str_replace('.','',$ids);
+	$player_cont = '<div class="'.$class.'">';
+	$player_cont .= '<input type="button" id="btnplay_'.$ids.'" class="myButton_play" onClick="play_mp3(\'play\',\''.$ids.'\',\''.$fileurl.'\');show_hide(\'play\',\''.$ids.'\');" />';
+	$player_cont .= '<input type="button"  id="btnstop_'.$ids.'" style="display:none" class="myButton_stop" onClick="play_mp3(\'stop\',\''.$ids.'\',\'\');show_hide(\'stop\',\''.$ids.'\');" />';
+ 	$player_cont .= '<div id="sm2-container"><!-- flash movie ends up here --></div>';
+ 	$player_cont .= '</div>';
+	return $player_cont;
+}
+add_shortcode('sc_embed_player', 'sc_embed_player_handler');
+if (!is_admin()){add_filter('widget_text', 'do_shortcode');}
+add_filter('the_excerpt', 'do_shortcode',11);
+
+//Create admin page 
+add_action('admin_menu', 'scap_mp3_player_admin_menu');
+function scap_mp3_player_admin_menu() {
+	add_options_page('SC Audio Player', 'SC Audio Player', 8, __FILE__, 'scap_mp3_options');
+}
+
+function scap_mp3_options() 
+{
+	echo '<div class="wrap">';
+	echo '<div id="icon-upload" class="icon32"><br></div><h2>SC Audio Player</h2><br />';
+
+	echo '<p>Visit the <a href="http://www.tipsandtricks-hq.com/?p=4556" target="_blank">Compact Audio Player</a> plugin page for documentation and update.</p>';
+	echo "<p>This is a Simple All Browser Supported Audio Player. There is no extra settings. Just add the shortcode with the MP3 file URL in a WordPress post or page to embed the audio player.</p>";
+	echo "<h3>Shortcode Format</h3>";
+	echo '<p><code>[sc_embed_player fileurl="URL OF THE MP3 FILE"]</code></p>';	
+	echo '<p><strong>Example:</strong></p>';
+	echo '<p><code>[sc_embed_player fileurl="http://www.example.com/wp-content/uploads/my-music/mysong.mp3"]</code></p>';
+	echo '</div>';
+}
+?>
